075fcdc9176f9476a20a3b4bdf49f84a216a5430
rogue-geek <84265206+rogue-geek@users.noreply.github.com>
2021-09-07 11:25:09 +0530

    Added prompts for Exp16

---
+++ Assignments/employee_in_16.c
@@ -0,0 +1,79 @@
+// Author: Gokul Raj, 235, R2A, CSE
+
+// Experiment 16 - Print employee details using structure.
+#include <stdio.h>
+
+#define ARR_LEN 256
+
+// Define a "employee" structure 
+struct employee
+{
+    char name[ARR_LEN];
+    int age;
+    char gender;
+    int salary;
+    char post[ARR_LEN];
+    int id;
+};
+
+// function to display an "employee" structure data
+void show_emp(struct employee e){
+    // print data
+    printf("\nName: %s\nAge: %d\nID: %d\nDesignation: %s\nGender: %c\nSalary: %d\n\n", e.name, e.age, e.id, e.post, e.gender, e.salary);
+}
+
+void main(){
+    // define array of employee structs
+    struct employee list[ARR_LEN];
+    int n, ind, str_i = 0;
+
+    // start struct data input
+    printf("Enter employee count: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++){
+        printf("\nEnter details of Employee %d\n-----\n", i+1);
+
+        printf("Name: ");
+        // flush buffer to fix prev. newline induced prompt bypass
+        fflush(stdin);
+        fgets(list[i].name, ARR_LEN, stdin);
+        // routine to remove residual new line char in name input
+        while(list[i].name[++str_i] != '\n');
+        list[i].name[str_i] = '\0';
+
+        printf("ID: ");
+        scanf("%d", &list[i].id);
+
+        printf("Age: ");
+        scanf("%d", &list[i].age);
+
+        printf("Gender: ");
+        // flush buffer to prevent premature closing of prompt
+        fflush(stdin);
+        scanf("%c", &list[i].gender);
+
+        printf("Salary: ");
+        scanf("%d", &list[i].salary);
+
+        printf("Designation: ");
+        // flush buffer again to fix prev. newline induced prompt bypass
+        fflush(stdin);
+        fgets(list[i].post, ARR_LEN, stdin);
+        // routine to remove residual new line char in designation input
+        str_i = 0;
+        while(list[i].post[++str_i] != '\n');
+        list[i].post[str_i] = '\0';
+    }
+
+    // query array of struct for data
+    while(1){
+        printf("\nEnter employee index to pull: ");
+        scanf("%d", &ind);
+        if(ind <= n && ind > 0)
+            show_emp(list[ind-1]);
+        else
+            printf("Invalid Index!!");
+    }
+    
+}
--- Tools/ccompile.bat
+++ Tools/ccompile.bat
@@ -1,7 +1,11 @@
 @echo off
+set loc=%~dp1
+
+mkdir %loc%\bin
 if %~x1==.c (
     echo Compiling %~f1
-    gcc %~f1 -o %~n1.exe
+    gcc %~f1 -o %loc%\bin\%~n1.exe
+    %loc%\bin\%~n1.exe
 ) else (
     echo Please pass a .c file to compile.
 )
